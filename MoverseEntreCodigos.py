8ff2597be66970fcbc20d564f6aab23ce17d965c
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-06-10 11:21:48 -0500

    Impresion con for

--- MoverseEntreCodigos.py
+++ MoverseEntreCodigos.py
@@ -3,3 +3,7 @@ from Funciones import square
 #Imprimir 
 x = 10
 print (f"El cuadrado que tiene {x} es {square(x)}")
+
+#Imprimit con un for
+for i in range (10):
+    print (f"El cuadrado con dimension {i} es igual a {square(i)}")
