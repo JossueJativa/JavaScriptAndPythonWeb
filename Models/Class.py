8b3eea294d070d5df918dee55a455742fd8b1e94
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-06-10 11:40:39 -0500

    Se a agregado funciones con sus metodos

--- Models/Class.py
+++ Models/Class.py
@@ -6,7 +6,33 @@ class Point():
         #Como se le asigna un valor a y
         self.y = y
 
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    ##agregar mefotos a las funciones
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        else:
+            self.passengers.append(name)
+            return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
 p = Point(3, 5)
 
 print (p.x)
 print (p.y)
+
+#Crear un vuelo con 3 asientos
+flight = Flight(3)
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    succes = flight.add_passenger(person)
+    if succes:
+        print (f"Se agrego a la persona {person} al vuelo con exito")
+    else:
+        print (f"No hay asientos disponibles para la persona {person}")
