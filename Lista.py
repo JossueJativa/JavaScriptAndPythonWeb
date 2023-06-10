f3e303e445558a05db1f254e4f4db1a4344e9df4
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-06-09 20:51:24 -0500

    Ingreso de nuevo programa

---
+++ Lista.py
@@ -0,0 +1,9 @@
+#Crear una lista de nombres
+nombres = ["Jossue", "Andres", "Luis", "Antonio"]
+
+#Agregar elementos a la lista
+nombres.append("Sebas")
+#sort es para poner elementos aleatorio en el programa
+nombres.sort()
+#imprimir la lista sin sort
+print(nombres)
